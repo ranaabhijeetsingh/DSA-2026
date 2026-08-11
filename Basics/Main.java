ece6ea1eeae10999d80c9dbf90ba70e88d040fd2
Rana <109157770+ranaabhijeetsingh@users.noreply.github.com>
2026-08-11 03:56:53 +0000

    Q:1.1 modified.

--- Basics/Main.java
+++ Basics/Main.java
@@ -1,7 +1,7 @@
    
 /* 
     Note: This questions are inherited for book Creacking the coding interview for only learning purpose.
-    
+
    1.1   Is Unique: Implement an algorithm to determine if a string has all unique characters.What if you
          cananot use additional data structure.
 
@@ -13,6 +13,15 @@
          alphabet. After all, you can't form a string of 280 unique characters out of a 128-character alphabet.
 
          It's also okay to assume 256 characters. This would be the case in extended ASCII. You should clarify your assumptions with your interviewr.
+
+         Time complexcity: O(n)
+         Space complexcity: O(1)
+         You could express the complexcity as O(c) spaceand O(min(Cj n)) or O(c) time, where c is the size of the character set.
+
+         We can reduce our space usage by a factor of eight by using a bit vector. We will assume, in the below code,
+         that the string only uses the lowercase letters a through z. This will allow us to use just a single int .
+
+         boolean
         
 */
 public class Main{
@@ -28,6 +37,19 @@ public class Main{
                 return false;
             }
             charSet[val] = true;
+        }
+        return true;
+    }
+
+// Optimize version
+    static boolean isUniqueCahrs(String str){
+        int checker = 0;
+        for(int i = 0; i < str.length(); i++){
+            int val  = str.charAt(i) - a;
+            if((checker & (1 << val)) > 0){
+                return false;
+            }
+            checker != (1 << val);
         }
         return true;
     }
